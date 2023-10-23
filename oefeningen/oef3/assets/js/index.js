9144cb271ea37427f963960221133bebaca7a8a6
Neverlanders <vanhoutte.tom@gmail.com>
2023-10-23 16:13:49 +0200

    oef3

--- oefeningen/oef3/assets/js/index.js
+++ oefeningen/oef3/assets/js/index.js
@@ -6,6 +6,9 @@
     document.getElementById('resultaat').innerText = '1';
 } else if (exponent < 0) {
     var resultaat = 1;
+    //de absolute waarde van een getal is de afstand van dat getal to nul op de getallenas.
+      //  Het is altijd een niet negatief getal
+        //bijvoorbeeld, de absolute waarde van -5 is 5. de absolute waarde van 5 is ook 5.
     for (var i = 0; i < Math.abs(exponent); i++) {
     resultaat /= grondtal;
 }
