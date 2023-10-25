62e56b1f712078049e6493267ee88ef21fd456e1
Neverlanders <vanhoutte.tom@gmail.com>
2023-10-25 09:34:42 +0200

    oef6 checkspaties

--- oefeningen/oef6/assets/js/index.js
+++ oefeningen/oef6/assets/js/index.js
@@ -3,6 +3,11 @@ function checkSpaties() {
     var inputTekst = document.getElementById('palindroom').value;
 
     // Verwijder de spaties uit de tekst
+    // /\s/ zoekt naar elk teken in de string dat overeenkomt met een witruimtekarakter. Hier omvat witruimte karakters zoals spaties, tabs en nieuwe regels.
+    //
+    // g is een zogenaamde "flag" die aangeeft dat de reguliere expressie globaal moet zoeken in de hele string. Zonder deze "g"-flag zou alleen het eerste overeenkomende witruimtekarakter worden vervangen.
+    //
+    // '' is de vervangende waarde. In dit geval is het een lege string, wat betekent dat de witruimte wordt vervangen door niets. Dus in feite wordt de witruimte verwijderd.
     var tekstZonderSpaties = inputTekst.replace(/\s/g, '');
     console.log(tekstZonderSpaties);
 
